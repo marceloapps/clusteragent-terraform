1a99aea97dd9ff2e1d2f6c6ede5408d20ab2b80e
Marcelo Arakaki Guimaraes <marcelo.arakaki@gmail.com>
2021-12-27 23:35:17 -0300

    wip

--- appdynamics.tf
+++ appdynamics.tf
@@ -11,9 +11,55 @@ resource "kubernetes_namespace" "appdynamics" {
   }
 }
 
+data "helm_template" "appdynamics" {
+  name       = "k8s"
+  namespace  = "appdynamics"
+  repository = "https://ciscodevnet.github.io/appdynamics-charts"
+
+  chart   = "cluster-agent"
+
+  templates = [
+    "templates/cluster-agent.yaml",
+  ]
+
+  set {
+    name  = "metadata.name"
+    value = "xpto"
+  }
+}
+
+resource "local_file" "appdynamics_manifests" {
+  for_each = data.helm_template.appdynamics.manifests
+
+  filename = "./${each.key}"
+  content  = each.value
+}
+
+data "kustomization_overlay" "appdynamics" {
+  resources = [
+    "cluster=agent.yaml",
+  ]
+
+  patches {
+    target = {
+      kind = "Clusteragent"
+      name = {{ printf "%s-%s" .Release.Name "appdynamics-cluster-agent" | trunc 63 }}
+    }
+    patch = <<-EOF
+      kind: Namespace
+      metadata:
+        name: new-ns
+    EOF
+    options {
+      allow_name_change = true
+    }
+  }
+
+}
+
 # Deploy the AppDynamics cluster agent using AppDynamic's helm chart
-resource "helm_release" "appdynamics-charts" {
-  name = "cluster-agent"
+resource "helm_release" "appdynamics" {
+  name = "k8s"
   namespace = "appdynamics"
   repository = "https://ciscodevnet.github.io/appdynamics-charts"
   chart = "cluster-agent"
